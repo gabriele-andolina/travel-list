00ff5a4646e2482d4b3196f9526505d7d9f647ac
gabriele-andolina <gabe.andolina@gmail.com>
2023-09-10 21:24:47 +0200

    Components: Added app title to Logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
-export default function App() {}
+export default function App() {
+    return (
+        <div>
+            <Logo />
+        </div>
+    );
+}
 
-function Logo() {}
+function Logo() {
+    return <h1>🏝 Far Away 🧳</h1>;
+}
 function Form() {}
 function PackingList() {}
 function Stats() {}
