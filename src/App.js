fe5870b5ff9cce67a27f6affa13224e4ab343a45
gabriele-andolina <gabe.andolina@gmail.com>
2023-09-10 21:26:37 +0200

    Components: Added title to Form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ export default function App() {
     return (
         <div>
             <Logo />
+            <Form />
         </div>
     );
 }
@@ -9,6 +10,12 @@ export default function App() {
 function Logo() {
     return <h1>🏝 Far Away 🧳</h1>;
 }
-function Form() {}
+function Form() {
+    return (
+        <div className="add-form">
+            <h3>What do you need for your trip?</h3>
+        </div>
+    );
+}
 function PackingList() {}
 function Stats() {}
