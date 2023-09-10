4788971e2c1b9ae73cb24d29f5f54e1504cd0152
gabriele-andolina <gabe.andolina@gmail.com>
2023-09-10 22:25:30 +0200

    Components: Created Item and added iteration through initialItems in PackingList

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+const initialItems = [
+    { id: 1, description: "Passports", quantity: 2, packed: false },
+    { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
 export default function App() {
     return (
         <div className="app">
@@ -20,8 +25,28 @@ function Form() {
     );
 }
 function PackingList() {
-    return <div className="list">List</div>;
+    return (
+        <div className="list">
+            <ul>
+                {initialItems.map((item) => (
+                    <Item item={item} />
+                ))}
+            </ul>
+        </div>
+    );
 }
+
+function Item({ item }) {
+    return (
+        <li>
+            <span>
+                {item.quantity} {item.description}
+            </span>
+            <button>❌</button>
+        </li>
+    );
+}
+
 function Stats() {
     return (
         <footer className="stats">
