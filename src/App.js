8e8ae3a0b0535f28b8572d9dab8896246f11cb6d
gabriele-andolina <gabe.andolina@gmail.com>
2023-09-12 16:04:21 +0200

    State: Added description and quantity variables to set S. based on user input

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
     { id: 1, description: "Passports", quantity: 2, packed: false },
     { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -19,17 +21,32 @@ function Logo() {
     return <h1>🏝 Far Away 🧳</h1>;
 }
 function Form() {
+    const [description, setDescription] = useState("");
+    const [quantity, setQuantity] = useState(1);
+
+    function handleSubmission(e) {
+        e.preventDefault();
+    }
+
     return (
-        <form className="add-form">
+        <form className="add-form" onSubmit={handleSubmission}>
             <h3>What do you need for your trip?</h3>
-            <select>
+            <select
+                value={quantity}
+                onChange={(e) => setQuantity(Number(e.target.value))}
+            >
                 {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
                     <option value={num} key={num}>
                         {num}
                     </option>
                 ))}
             </select>
-            <input type="text" placeholder="Item..."></input>
+            <input
+                type="text"
+                placeholder="Item..."
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+            ></input>
             <button>Add</button>
         </form>
     );
