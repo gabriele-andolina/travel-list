f04a1e9a3117a9e42956e07742d93675085b0c44
gabriele-andolina <gabe.andolina@gmail.com>
2023-09-17 00:18:26 +0200

    Elements: Added early return if no items are present in the list

--- src/App.js
+++ src/App.js
@@ -117,6 +117,13 @@ function Item({ item, onDeleteItem, onToggleItem }) {
 }
 
 function Stats({ items }) {
+    if (!items.length)
+        return (
+            <p className="stats">
+                Start adding some items to your packing list.
+            </p>
+        );
+
     const numItems = items.length;
     const numPacked = items.filter((item) => item.packed).length;
     const percentage = Math.round((numPacked / numItems) * 100);
