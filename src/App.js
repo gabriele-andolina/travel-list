231db0cee19b6757270e476b712ee39ff6cc77e9
gabriele-andolina <gabe.andolina@gmail.com>
2023-09-18 19:56:37 +0200

    Logic: Added sorting options in PackingList

--- src/App.js
+++ src/App.js
@@ -84,10 +84,26 @@ function Form({ onAddItems }) {
     );
 }
 function PackingList({ items, onDeleteItem, onToggleItem }) {
+    const [sortBy, setSortBy] = useState("input");
+
+    let sortedItems;
+
+    if (sortBy === "input") sortedItems = items;
+
+    if (sortBy === "description")
+        sortedItems = items
+            .slice()
+            .sort((a, b) => a.description.localeCompare(b.description));
+
+    if (sortBy === "packed")
+        sortedItems = items
+            .slice()
+            .sort((a, b) => Number(a.packed) - Number(b.packed));
+
     return (
         <div className="list">
             <ul>
-                {items.map((item) => (
+                {sortedItems.map((item) => (
                     <Item
                         item={item}
                         key={item.id}
@@ -98,7 +114,10 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
             </ul>
 
             <div className="actions">
-                <select>
+                <select
+                    value={sortBy}
+                    onChange={(e) => setSortBy(e.target.value)}
+                >
                     <option value="input">Sort by Input Order</option>
                     <option value="description">Sort by Description</option>
                     <option value="packed">Sort by Packed Status</option>
