da6566feb7a7f135a25d02f5d2b261bffbfe9e36
gabriele-andolina <gabe.andolina@gmail.com>
2023-09-19 14:09:13 +0200

    Refactor: Moved components to individual files and separate folder

---
+++ src/components/Stats.js
@@ -0,0 +1,23 @@
+export default function Stats({ items }) {
+    if (!items.length)
+        return (
+            <p className="stats">
+                Start adding some items to your packing list.
+            </p>
+        );
+
+    const numItems = items.length;
+    const numPacked = items.filter((item) => item.packed).length;
+    const percentage = Math.round((numPacked / numItems) * 100);
+    return (
+        <footer className="stats">
+            <em>
+                {percentage === 100
+                    ? "You've got everything. Ready to go! 🛫"
+                    : `💼You have ${numItems} items on your list, and you've already
+                packed ${numPacked} (${percentage}%)`}
+                .
+            </em>
+        </footer>
+    );
+}
